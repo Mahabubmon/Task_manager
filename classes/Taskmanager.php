1528e6d66e7ad4f936847d09af93854d86e44c77
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-14 19:03:17 +0600

    some_error

--- classes/Taskmanager.php
+++ classes/Taskmanager.php
@@ -20,34 +20,34 @@
             
         }
         
-       
-    }
-       // All Task
-       public function allTask(){
-        return $result = $this->con->query("SELECT * FROM `tasks`");
-      }
-
-      // edit task
-      public function edit_task($tid){
-        return $result =  $this->con->query("SELECT * FROM `tasks` WHERE id ='$tid'");
-
-      }
-      // update task
-      public function update_task($allTask,$tid){
-        $taskname = $allTask['taskName'];
-        $taskdate = $allTask['taskdate'];
-
-        $sql = "UPDATE `tasks` SET `task_name`='$taskname',`task_date`='$taskdate' WHERE id='$tid' ";
-
-        $result = $this->con->query($sql);
-
-        if($result){
-            header("Location:index.php");
+        // All Task
+        public function allTask(){
+          return $result = $this->con->query("SELECT * FROM `tasks`");
         }
 
+        // edit task
+        public function edit_task($tid){
+           return $result =  $this->con->query("SELECT * FROM `tasks` WHERE id ='$tid'");
+        
+
+        }
+        // update task
+        public function update_task($allTask,$tid){
+            $taskname = $allTask['taskName'];
+            $taskdate = $allTask['taskdate'];
+
+            $sql = "UPDATE `tasks` SET `task_name`='$taskname',`task_date`='$taskdate' WHERE id='$tid' ";
+
+            $result = $this->con->query($sql);
+
+            if($result){
+                header("Location:index.php");
+            }
+
+
+
+        }
 
     }
-
-
 
 ?>
