e20d9e7efba0540039c12a3eb752dd471595a63b
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-14 19:50:37 +0600

    data_fetch_into_array

--- classes/edit.php
+++ classes/edit.php
@@ -3,5 +3,11 @@
     $u1 = new TaskManager();
 
     $id = $_GET['id'];
+    if(isset($_GET['id'])){
+        $data = $u1->edit_task($id);
+        $row = mysqli_fetch_assoc($data);
+        
+
+    }
 
 ?>
